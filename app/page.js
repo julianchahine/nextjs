093b39b194ec535e0b69fec99eae22230de00051
Julian Chahine <j.chahine75@gmail.com>
2023-07-29 12:54:59 +0200

    hehe

--- app/page.js
+++ app/page.js
@@ -1,7 +1,6 @@
 import styles from './page.module.css'
 import './globals.css'
 
-import sliderImage from './_images/sliderImage.webp'
 import circlesImage from './_images/circles.svg'
 import webDevImage from './_images/webDev.svg'
 import iMacsImage from './_images/imacs.webp'
@@ -30,7 +29,7 @@ export default function Home() {
                         </h2>
                     </div>
                     <div className={styles.sliderRight}>
-                        <Image src={sliderImage} className={styles.sliderImage} width="600" height="500" alt="" title="" />
+                        <img src="https://testproject-28x3emayd-jchahine75-gmailcom.vercel.app/_next/image?url=%2F_next%2Fstatic%2Fmedia%2FsliderImage.6845baa2.webp&w=640&q=75" className={styles.sliderImage} width="600" height="500" alt="" title="" />
                     </div>
                 </div>
                 <Image src={circlesImage} className={styles.circlesImage} width="50" height="50" alt="" title="" />
